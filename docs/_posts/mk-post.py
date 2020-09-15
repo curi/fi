dda4ee355a37282269f0c0f0b53a07d8d1d0c947
Max Kaye <m@xk.io>
2020-09-15 18:31:01 +1000

    add microblog post

--- docs/_posts/mk-post.py
+++ docs/_posts/mk-post.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+from prompt_toolkit import prompt
+from prompt_toolkit.validation import Validator
+from prompt_toolkit.completion import WordCompleter
+import json
+import inflection
+
+
+def is_bool(v):
+    return v.lower() in ['true', 'false']
+
+
+yesno_validator = Validator.from_callable(
+    is_bool,
+    error_message="Enter 'yes' or 'no'",
+    move_cursor_to_end=True)
+
 
 d = datetime.now()
 ic = d.isocalendar()
@@ -8,14 +24,17 @@ iso_format = d.isoformat().split('T')
 iso_date = iso_format[0]
 iso_time = ':'.join(iso_format[1].split(':')[0:2])
 
-raise Exception('todo title')
+title = prompt("Title of the post: ")
+cats_ = prompt("Catagories, space separated (or empty): ")
+cats = f"categories: {json.dumps(cats_)}" if len(cats_) > 0 else ""
+
 
 frontmatter = f"""---
-title: " __TITLE__ "
+title: {json.dumps(title)}
 layout: post
 date: {iso_date} {iso_time}
-parent: Learning Reports
-category: Learning Reports
+parent: Posts
+{cats}
 ---
 """
 
@@ -23,7 +42,7 @@ import os
 from pathlib import Path
 
 p = Path(__file__)
-new_lr_file = p.parent / f"{iso_date}-{todo_title}.md"
+new_lr_file = p.parent / f"{iso_date}-{inflection.parameterize(title, separator='-')}.md"
 
 if os.path.exists(new_lr_file):
     raise Exception(f"Planned to create {new_lr_file} but a file already exists there!")
@@ -37,3 +56,7 @@ print(f"""Wrote the below frontmatter to {new_lr_file}
 
 You can now edit {new_lr_file} as required.
 """)
+
+start_vscode = bool(prompt(f"Run `code {new_lr_file}`? (yes/no) > ", validator=yesno_validator, completer=WordCompleter({'yes', 'no'})).lower())
+if start_vscode:
+    os.system(f"code {new_lr_file}")
